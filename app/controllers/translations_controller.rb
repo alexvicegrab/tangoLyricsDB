d072f599b200149a89fae065ae89207a52d9c107
Alejandro (Sasha) Vicente Grabovetsky <a.vicente.grab@gmail.com>
2014-04-29 23:06:15 +0200

    Improved error reporting

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -4,15 +4,21 @@ class TranslationsController < ApplicationController
   
   def create
     @song = Song.find(params[:song_id])
-    @translation = @song.translations.create(translation_params)
-    redirect_to song_path(@song)
+    @translation = @song.translations.new(translation_params)
+    if @translation.save
+      flash[:translation_success] = "Thank you for adding a new translation!"
+    else
+      flash[:translation_error] = @translation.errors.empty? ? "Error" : @translation.errors.full_messages.to_sentence
+    end
+    redirect_to @song
+    
   end
     
   def destroy
     @song = Song.find(params[:song_id])
     @translation = @song.translations.find(params[:id])
     @translation.destroy
-    redirect_to song_path(@song)
+    redirect_to @song
   end
  
   private
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,3 +1,8 @@
 class Translation < ActiveRecord::Base
   belongs_to :song
+  
+  validates :link,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 15 }
 end
