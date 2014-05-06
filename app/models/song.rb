1bc327befb11ba6fd58b533f88d62e8e0000ddd2
Alejandro (Sasha) Vicente Grabovetsky <a.vicente.grab@gmail.com>
2014-05-06 02:33:54 +0200

    Added pre-validation Callback to titleise songs parameters before saving them + remove timestamp & id from index

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,21 +1,29 @@
 class Song < ActiveRecord::Base
   has_many :translations, dependent: :destroy
   
-  default_scope { order('title DESC') }
+  default_scope { order('title') }
   
   validates :title,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    length: { minimum: 4 }
+  presence: true,
+  uniqueness: { case_sensitive: false },
+  length: { minimum: 4 }
     
   validates :year,
-    numericality: {only_integer: true, greater_than_or_equal_to: 1866, less_than_or_equal_to: 2014}
+  numericality: {only_integer: true, greater_than_or_equal_to: 1866, less_than_or_equal_to: 2014}
     
   validates :composer,
-    presence: true,
-    length: { minimum: 4 }
+  presence: true,
+  length: { minimum: 4 }
       
   validates :lyricist,
-    presence: true,
-    length: { minimum: 4 }
+  presence: true,
+  length: { minimum: 4 }
+    
+  before_validation :normalise_title, on: [ :create, :update ]
+    
+  protected
+  def normalise_title
+    self.title = self.title.downcase.titleize
+  end
+  
 end
