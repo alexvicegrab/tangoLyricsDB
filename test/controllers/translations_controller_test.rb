64cfa613b1f41b71bb4676b40c21c7af4ce1ae05
Alejandro (Sasha) Vicente Grabovetsky <a.vicente.grab@gmail.com>
2014-06-18 12:16:55 +0200

    Finished basic controller unit tests

--- test/controllers/translations_controller_test.rb
+++ test/controllers/translations_controller_test.rb
@@ -2,10 +2,8 @@ require 'test_helper'
 
 class TranslationsControllerTest < ActionController::TestCase
   setup do
-    #@song = songs(:one)
-    #@translation = @song.translations.first
+    @song = songs(:one)
     @translation = translations(:one)
-    get :index, song_id: 1
   end
 
   test "should get index" do
@@ -14,43 +12,38 @@ class TranslationsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:translations)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # removed 'should get new' test
 
   test "should create translation" do
     assert_difference('Translation.count') do
-      post :create, id: 1, translation: { link: 'http://translationLink',
-      language_id: 1,
-      song_id: 1 }
+      post :create, id: 1, song_id: @song, translation: { link: 'http://translationLink',
+      language_id: 1 }
     end
 
-    assert_redirected_to translation_path(assigns(:translation))
+    assert_redirected_to song_path(@song)
   end
 
   test "should show translation" do
-    get :show, id: @translation
+    get :show, id: @translation, song_id: @translation.song_id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @translation
+    get :edit, id: @translation, song_id: @translation.song_id
     assert_response :success
   end
 
   test "should update translation" do
-    patch :update, id: @translation, translation: { link: @translation.link,
-    language_id: @translation.language_id,
-    song_id: @translation.song_id }
-    assert_redirected_to translation_path(assigns(:translation))
+    patch :update, id: @translation, song_id: @translation.song_id, translation: { link: @translation.link,
+    language_id: @translation.language_id }
+    assert_redirected_to song_path(@translation.song_id)
   end
 
   test "should destroy translation" do
     assert_difference('Translation.count', -1) do
-      delete :destroy, id: @translation
+      delete :destroy, id: @translation, song_id: @translation.song_id
     end
 
-    assert_redirected_to translations_path
+    assert_redirected_to song_path(@translation.song_id)
   end
 end
