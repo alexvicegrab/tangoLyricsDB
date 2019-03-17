74e44cf7285d4123b667a58c333465071750fc5d
Alejandro Vicente Grabovetsky <sasha@aid.technology>
2019-03-17 07:35:35 +0200

    Temporarily deactivate NGINX until we figure out what's wrong with JS
    Signed-off-by: Alejandro Vicente Grabovetsky <sasha@aid.technology>

--- rails_app/config/environments/production.rb
+++ rails_app/config/environments/production.rb
@@ -35,14 +35,16 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or NGINX will already do this).
-  config.serve_static_files = false
+  # TODO: Set to false once NGINX works
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # TODO: Set to false once NGINX works
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
